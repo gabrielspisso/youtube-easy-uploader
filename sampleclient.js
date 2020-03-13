be7d961dc761b2960a29cc67e4edc1e3fb076952
gabrielspisso <gabrielhspisso@gmail.com>
2020-03-13 01:20:44 -0300

    Inicio de servidor

--- sampleclient.js
+++ sampleclient.js
@@ -28,7 +28,7 @@ const path = require('path');
 
 const keyPath = path.join(__dirname, 'oauth2.keys.json');
 let keys = {
-  redirect_uris: ['http://localhost:3000/oauth2callback'],
+  redirect_uris: ['http://localhost:8080/oauth2callback'],
 };
 if (fs.existsSync(keyPath)) {
   const keyFile = require(keyPath);
@@ -37,10 +37,10 @@ if (fs.existsSync(keyPath)) {
 
 const invalidRedirectUri = `The provided keyfile does not define a valid
 redirect URI. There must be at least one redirect URI defined, and this sample
-assumes it redirects to 'http://localhost:3000/oauth2callback'.  Please edit
+assumes it redirects to 'http://localhost:8080/oauth2callback'.  Please edit
 your keyfile, and add a 'redirect_uris' section.  For example:
 "redirect_uris": [
-  "http://localhost:3000/oauth2callback"
+  "http://localhost:8080/oauth2callback"
 ]
 `;
 
@@ -53,10 +53,11 @@ class SampleClient {
       throw new Error(invalidRedirectUri);
     }
     const redirectUri = keys.redirect_uris[keys.redirect_uris.length - 1];
+    console.log("LA URLLLL",redirectUri)
     const parts = new url.URL(redirectUri);
     if (
       redirectUri.length === 0 ||
-      parts.port !== '3000' ||
+      parts.port !== '8080' ||
       parts.hostname !== 'localhost' ||
       parts.pathname !== '/oauth2callback'
     ) {
@@ -81,29 +82,33 @@ class SampleClient {
         access_type: 'offline',
         scope: scopes.join(' '),
       });
-      const server = http
-        .createServer(async (req, res) => {
-          try {
-            if (req.url.indexOf('/oauth2callback') > -1) {
-              const qs = new url.URL(req.url, 'http://localhost:3000')
-                .searchParams;
-              res.end(
-                'Authentication successful! Please return to the console.'
-              );
-              server.destroy();
-              const {tokens} = await this.oAuth2Client.getToken(qs.get('code'));
-              this.oAuth2Client.credentials = tokens;
-              resolve(this.oAuth2Client);
-            }
-          } catch (e) {
-            reject(e);
-          }
-        })
-        .listen(3000, () => {
-          // open the browser to the authorize url to start the workflow
-          opn(this.authorizeUrl, {wait: false}).then(cp => cp.unref());
-        });
-      destroyer(server);
+      console.log(this.authorizeUrl)
+      resolve(this.authorizeUrl)
+      // const server = http
+      //   .createServer(async (req, res) => {
+      //     try {
+      //       if (req.url.indexOf('/oauth2callback') > -1) {
+      //         const qs = new url.URL(req.url, 'http://localhost:8080')
+      //           .searchParams;
+      //         res.end(
+      //           'Authentication successful! Please return to the console.'
+      //         );
+      //         server.destroy();
+      //         const {tokens} = await this.oAuth2Client.getToken(qs.get('code'));
+      //         this.oAuth2Client.credentials = tokens;
+      //         resolve(this.oAuth2Client);
+      //         console.log("EL RESPONSE TIENE:",res)
+      //         console.log("EL refresh_token TIENE:",res.refresh_token)              
+      //       }
+      //     } catch (e) {
+      //       reject(e);
+      //     }
+      //   })
+      //   .listen(8080, () => {
+      //     // open the browser to the authorize url to start the workflow
+      //     opn(this.authorizeUrl, {wait: false}).then(cp => cp.unref());
+      //   });
+      // destroyer(server);
     });
   }
 }
---
+++ upload.js
@@ -0,0 +1,74 @@
+// Copyright 2016 Google LLC
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+'use strict';
+
+/**
+ * Usage: node upload.js PATH_TO_VIDEO_FILE
+ */
+
+const fs = require('fs');
+const readline = require('readline');
+
+const {google} = require('googleapis');
+const sampleClient = require('./sampleclient');
+
+// initialize the Youtube API library
+const youtube = google.youtube({
+  version: 'v3',
+  auth: sampleClient.oAuth2Client,
+});
+
+// very basic example of uploading a video to youtube
+async function runSample(fileName) {
+
+  const fileSize = fs.statSync(fileName).size;
+  const res = await youtube.videos.insert(
+    {
+      part: 'id,snippet,status',
+      notifySubscribers: false,
+      requestBody: {
+        snippet: {
+          title: 'Node.js YouTube Upload Test',
+          description: 'Testing YouTube upload via Google APIs Node.js Client',
+        },
+        status: {
+          privacyStatus: 'public',
+        },
+      },
+      media: {
+        body: fs.createReadStream(fileName),
+      },
+    },
+    {
+      // Use the `onUploadProgress` event from Axios to track the
+      // number of bytes uploaded to this point.
+      onUploadProgress: evt => {
+        const progress = (evt.bytesRead / fileSize) * 100;
+        readline.clearLine(process.stdout, 0);
+        readline.cursorTo(process.stdout, 0, null);
+        process.stdout.write(`${Math.round(progress)}% complete`);
+      },
+    }
+  );
+  console.log('\n\n');
+  console.log(res.data);
+  return res.data;
+}
+
+
+
+module.exports = {
+  runSample,
+  //client: sampleClient.oAuth2Client,
+};
